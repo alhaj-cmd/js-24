e6280feab61dfd87159c8c551ea795df4cc07d1a
github_username <you@domain.com>
2022-09-23 12:27:40 +0600

    second commit

---
+++ child.js
@@ -0,0 +1,7 @@
+const createElement = document.getElementById('content-add');
+const li = document.createElement('li');
+li.innerText='hello good affternoon';
+const h2 = document.createElement('h2');
+h2.innerText='i am boos';
+createElement.appendChild(h2);
+
